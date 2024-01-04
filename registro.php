3fced248a3094bc8ecea9c0f7bbba1edf87a553f
Franco <francombene@gmail.com>
2024-01-04 03:52:16 -0300

    Subiendo Bailaweb

---
+++ registro.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<br>
+<h1>BAILAWEB</h1>
+
+ <form method=POST action="indexlogin.html" class="formulario">
+    
+    <h1>REGISTRO DE USUARIO</h1>
+     <div class="contenedor">
+     
+     <div class="input-contenedor">
+        <i class="fas fa-user icon"></i>
+         <input type="text" placeholder="Ingrese Nombre">
+         </div>
+
+    <div class="input-contenedor">
+        <i class="fas fa-user-check icon"></i>
+         <input type="text" placeholder="Ingrese Apellido">
+        </div>
+    <div class="input-contenedor">
+        <i class="fas fa-envelope icon"></i>
+         <input type="email" placeholder="Ingrese Email">
+         </div>
+
+    <div class="input-contenedor">
+        <i class="fas fa-key icon"></i>
+         <input type="password" placeholder="Ingrese Contraseña">
+         </div>
+        
+         <input type="submit" value="Confirmar" class="button">
+         <br><br>
+
+         </form>
+         <form method=POST action="index.html">
+         <input type="submit" value="Volver" class="button2">
+         </form>
+         <p>¿Ya tenes una cuenta?<a class="link" href="index.html"> Iniciar Sesion </a></p>
+     </div>
+    </form>
+</body>
+</html>
